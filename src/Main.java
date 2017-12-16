6633cf152b3cf1f576c898d50580e2ba1840462b
Batzuhan <batuhan.ozcan@ozu.edu.tr>
2017-12-16 15:41:03 +0300

    Added listener to refresh database when user wants to close the frame.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,11 @@ import model.Library;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import static controller.LibraryController.handler;
+
 /**
  * Created by Murathan on 13/12/2017.
  */
@@ -26,5 +30,13 @@ public class Main {
         frame.add(controller.getView(), BorderLayout.CENTER);
         frame.setSize(500, 500);
         frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                handler.refreshDatabase(library);
+                System.exit(0);
+            }
+        });
+
     }
 }
